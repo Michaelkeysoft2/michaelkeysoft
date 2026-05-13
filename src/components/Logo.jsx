b1c0524e3c6d7850e5aa299be6cbec28229dfa87
Michaelkeysoft <michaelkeysoft@gmail.com>
2026-05-13 17:41:34 +0100

    Finalize brand logo: arc touches M leg, no bottom circle, pure white styling

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -8,29 +8,19 @@ const Logo = ({ className = "w-10 h-10", color = "currentColor" }) => {
       xmlns="http://www.w3.org/2000/svg"
       className={className}
     >
-      {/* Thinner outer circle */}
-      <circle 
-        cx="50" 
-        cy="50" 
-        r="45" 
-        stroke={color} 
-        strokeWidth="2" 
-        opacity="0.3"
-      />
-      
-      {/* Thick accent arc (from approx 210deg to 30deg) */}
+      {/* Partial arc - starts at the left leg and sweeps over to the right side */}
       <path 
-        d="M27.5 82.5 A 45 45 0 1 1 82.5 27.5" 
+        d="M20 80 A 42 42 0 1 1 85 65" 
         stroke={color} 
-        strokeWidth="8" 
+        strokeWidth="10" 
         strokeLinecap="round"
       />
       
       {/* Stylized 'M' */}
       <path 
-        d="M30 80 L50 40 L60 60 L70 40 L90 80" 
+        d="M20 80 L40 40 L50 60 L60 40 L80 80" 
         stroke={color} 
-        strokeWidth="6" 
+        strokeWidth="10" 
         strokeLinejoin="round" 
         strokeLinecap="round"
       />
